2f908dcc2b79d56a914216f1489e484f6e206181
Mauricio Machado da Rosa <mauricio.rosa@agrotools.com.br>
2025-07-01 00:57:37 -0300

    funcional carrinho interagindo com modulo de catalogo

--- src/ModularMonolith.Purchase/Application/UseCases/GetCartByUserUseCase.cs
+++ src/ModularMonolith.Purchase/Application/UseCases/GetCartByUserUseCase.cs
@@ -10,22 +10,32 @@ public interface IGetCartByUserUseCase
     Task<Result<GetCartByUserResponse, Error>> Execute(Guid userId);
 }
 
-public record CartItemDto(string ItemId, int Quantity);
-public record GetCartByUserResponse(string CartId, Guid UserId, DateTime CreatedAt, List<CartItemDto> Items);
+public record CartItemDto(string ItemId, string Name, string Description, int Quantity, decimal Price);
+public record GetCartByUserResponse(string CartId, List<CartItemDto> Items, decimal Total);
 
 internal sealed class GetCartByUserUseCase(ICartRepository cartRepository, IGetProductsSharedUseCase getProductsSharedUseCase) : IGetCartByUserUseCase
 {
     public async Task<Result<GetCartByUserResponse, Error>> Execute(Guid userId)
     {
         var cart = await cartRepository.GetByUserId(userId) ?? new Cart(userId);
-        var getProducts = await getProductsSharedUseCase.Execute(cart.Items.Select(i => i.ItemId).ToArray());
-        var items = cart.Items.Select(i => new CartItemDto(i.ItemId, i.Quantity)).ToList();
+        if (!cart.Items.Any())
+            return new GetCartByUserResponse(cart.Id.ToString(), [], 0);
 
+        var getProducts = await getProductsSharedUseCase.Execute(cart.Items.Select(i => i.ItemId).ToArray());
+        if (getProducts.IsFailure)
+            return new BadRequestError("Nao foi possivel obter os itens do carrinho");
+
+        var items = getProducts.Value!.Select(i =>
+        {
+            var quantity = cart.Items.FirstOrDefault(ci => ci.ItemId == i.Id)?.Quantity ?? 0;
+            return new CartItemDto(i.Id, i.Name, i.Description, quantity, i.Price);
+        }).ToList();
+
+        var total = items.Sum(i => i.Price * i.Quantity);
         return new GetCartByUserResponse(
             cart.Id.ToString(),
-            cart.UserId,
-            cart.CreatedAt,
-            items
+            items,
+            total
         );
     }
 }
